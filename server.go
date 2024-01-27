e82c285c7e2310eb25c8dd04af5096c3499a338b
Nesrux <joaomarcosdevs@gmail.com>
2024-01-27 18:21:17 -0300

    ci: Criando arquivo de volume para o kuberntes

--- server.go
+++ server.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configMap", configMap)
+
 	http.ListenAndServe(":8000", nil)
 
 }
@@ -16,4 +20,11 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 	w.Write([]byte("<h1>Hello Kubernets!!!</h1>"))
 	fmt.Fprintf(w, "Olá eu sou %s minha idade é %s", name, age)
+}
+func configMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Erro ao ler o arquivo: ", err)
+		fmt.Fprintf(w, "minha familia %s.", string(data))
+	}
 }
