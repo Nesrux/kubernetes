83b5976b57bfa017bf80602ec57ca014bfc243fc
Nesrux <joaomarcosdevs@gmail.com>
2024-01-29 17:45:59 -0300

    ci: config health check on kubernetes

--- server.go
+++ server.go
@@ -6,13 +6,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func main() {
 
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/configMap", configMap)
 	http.HandleFunc("/secret", secret)
+	http.HandleFunc("/helth", helth)
 	http.ListenAndServe(":8000", nil)
 
 }
@@ -33,4 +37,12 @@ func secret(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("USER")
 	pass := os.Getenv("PASS")
 	fmt.Fprintf(w, "USER : %s. -- PASS: %s", user, pass)
+}
+func helth(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v,", duration.Seconds())))
+	}
 }
